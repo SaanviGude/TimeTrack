be103fd6d9008cce3844eb903b6c8ada0fbd605d
chodankarsaish1617@gmail.com <chodankarsaish1617@gmail.com>
2025-08-04 15:25:05 +0530

    added user endpoints branch/saish

--- backend/app/database.py
+++ backend/app/database.py
@@ -14,3 +14,12 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
+# Database dependency function
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
